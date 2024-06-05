5a8005f2917ba8403978247c2ff8b42371cb7a19
samuelmugambi5877 <samuelmugambi5877@gmail.com>
2024-06-05 11:41:45 +0300

    Initial commit:added recipe_finder.py script

---
+++ recipe_finder.py
@@ -0,0 +1,8 @@
+# recipe_finder.py
+
+def print_welcome_message():
+    print("Welcome to the Recipe Finder")
+    print("You can search for recipes based on ingredients and preferences.")
+    print("Choose an option to get started:")
+
+print_welcome_message()
